d2843c9bfa5eac5e7ad96e4d353d781f61b8af56
thowardnj <tory215@gmail.com>
2020-01-16 15:07:45 +0000

    I've corrected the typo in line 6. Was ',' now '.'

--- ex8.py
+++ ex8.py
@@ -3,10 +3,10 @@ formatter = "{} {} {} {}"
 print(formatter.format(1,2,3,4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
-print(formatter,format(formatter, formatter, formatter, formatter))
+print(formatter.format(formatter, formatter, formatter, formatter))
 print(formatter.format(
-    "Try your",
-    "Own text here",
-    "Maybe a poem",
-    "Or a song about fear"
+    "The Streets give me stress",
+    "My addiction to cess",
+    "Led to my first conviction and arrest",
+    "Yes, Yes"
     ))
